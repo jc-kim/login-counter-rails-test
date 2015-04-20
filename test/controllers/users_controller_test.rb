74f0924aec68671986b50916ac9687279f2a1b52
Kim, Jae Chan <kjc0210@gmail.com>
2015-04-20 19:34:38 +0900

    add signup happy test

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
-  test 'not actually a test' do
-    assert_not false
+  test '#signup_success' do
+    post :signup, {'username' => 'testuser', 'password' => 'testpass'}
+    json = JSON.parse(response.body)
+    assert json['user_name'] == 'testuser'
+    assert json['login_count'] == 1
   end
 end
